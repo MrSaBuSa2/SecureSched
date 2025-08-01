8972e386da4efff229c18798672d98e9f3c77183
quentin.vaney@gmail.com <quentin@MacBook-Air-de-Quentin.local>
2025-08-01 15:52:55 +0200

    Safe CORS

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 # === CORS ===
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ⚠️ À restreindre en prod
+    allow_origins=["http://localhost:3000"],  # ⚠️ À restreindre en prod
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
